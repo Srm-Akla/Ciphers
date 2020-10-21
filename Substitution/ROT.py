2ccd7e58b7a5c7185c071870f7a03159836b23f3
Srm-Akla <akellamahateja@gmail.com>
2020-10-21 21:25:28 +0200

    Adding ROT.py

--- Substitution/ROT.py
+++ Substitution/ROT.py
@@ -1,5 +1,4 @@
 #!/bin/python3
-#ROT-5
 #ROT-13
 #ROT-47
 
@@ -7,32 +6,42 @@ import argparse
 
 class ROT:
     def __init__(self):
+
         parser = argparse.ArgumentParser()
-        parser.add_argument("-rot7", help="Enter Domain:", type=str, required=False)
-        parser.add_argument("-rot47", help="Enter text:", type=str, required=False)
+        parser.add_argument("-e", "--encrypt", help="Enter text for encryption:", type=str, required=False)
+        parser.add_argument("-d", "--decrypt", help="Enter text for decryption:", type=str, required=False)
+        parser.add_argument("-a", "--alpha", default=47, help="Enter text for decryption:", type=int, required=False)
         self.txt = parser.parse_args()
 
-    def rot7(self):
-        self.result = []
-        def encrypt()
-            for letter in self.txt.rot7:
-                if letter == chr(32):
-                    self.result.append(" ")
-                elif letter > chr(65) and letter < chr(90):
-                    self.result.append(chr((ord(letter)+7-65)%26 + 65))
-                elif letter > chr(97) and letter < chr(122):
-                    self.result.append(chr((ord(letter)+7-97)%26 + 97))
-            print("".join(self.result))
-        def decrypt():
-            for letter in self.txt.rot7:
-                if letter == chr(32):
-                    self.result.append(" ")
-                elif letter > chr(65) and letter < chr(90):
-                    self.result.append(chr((ord(letter)+(26-7)-65)%26 + 65))
-                elif letter > chr(97) and letter < chr(122):
-                    self.result.append(chr((ord(letter)+(26-7)-97)%26 + 97))
-            print("".join(self.result))
+        self.result= []
+        self.key = 13
+
+        if self.txt.encrypt:
+            print(self.encrypt())
+        elif self.txt.decrypt:
+            print(self.decrypt())
+        else:
+            print(self.txt.alpha)
+
+    def encrypt(self):
+        for letter in self.txt.encrypt:
+            if letter == chr(32):
+                self.result.append(" ")
+            elif letter > chr(65) and letter < chr(90):
+                self.result.append(chr((ord(letter)+self.key-65)%26 + 65))
+            elif letter > chr(97) and letter < chr(122):
+                self.result.append(chr((ord(letter)+self.key-97)%26 + 97))
+        return "".join(self.result)
+
+    def decrypt(self):
+        for letter in self.txt.decrypt:
+            if letter == chr(32):
+                self.result.append(" ")
+            elif letter > chr(65) and letter < chr(90):
+                self.result.append(chr((ord(letter)+(26-self.key)-65)%26 + 65))
+            elif letter > chr(97) and letter < chr(122):
+                self.result.append(chr((ord(letter)+(26-self.key)-97)%26 + 97))
+        return "".join(self.result)
 
 if __name__ == "__main__":
     rot = ROT() 
-    print(rot.rot7())
